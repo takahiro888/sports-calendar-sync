8b226a07c3d9c59d24f8f12f51effcb28fdd520d
takahiro888 <takahiro.45666@gmail.com>
2026-08-13 22:31:23 +0900

    fix: 必須プロパティ関連エラー対応

--- src/types/sync.ts
+++ src/types/sync.ts
@@ -2,7 +2,7 @@ export type SyncItem = {
   id: string;
   title: string;
   description: string;
-  subDescription: string;
+  subDescription?: string;
   badge?: string;
   disabled?: boolean;
   icalUrl?: string;
